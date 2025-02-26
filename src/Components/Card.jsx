0cd6c5fe22bb76b8ffa989ff9ef4f5542b677118
vj <139743089+Vinayak-Joshi15@users.noreply.github.com>
2025-02-27 01:23:55 +0530

    Fixed the cards and API bugs

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function Card({name,id,image}){
+export default function Card({name,id,image,call}){
     console.log(name,id);
     return(
-        <div className="grid grid-cols-3 gap-4 p-4">
-        <div key={id} className="bg-white shadow-lg rounded-lg p-4">
+        <div className="grid grid-cols-2 gap-4 p-4">
+        <div key={id} className="bg-white shadow-lg rounded-lg p-4" onClick={call}>
           <img src={image} alt={name} className="w-full h-32 object-cover rounded-md" />
           <h2 className="text-xl font-bold mt-2">{name}</h2>
         </div>
